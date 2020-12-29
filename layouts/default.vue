8355c754252f98b3b0e0692d50aa2b6223c6211f
jarita <jarita0826@gmail.com>
2020-12-29 15:22:23 +0800

    add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,63 @@
       <div class="text-white p-6 border-b-2 border-text">
         <div class="flex container mx-auto justify-between list-none">
           <div class="flex font-bold space-x-4">
-            <li v-for="(item, index) in menu" :key="index">
-              <nuxt-link to="/">{{ item }}</nuxt-link>
+            <div class="dropbtn">
+              <li @click="clearViewModel">
+                <nuxt-link to="/">BOOK</nuxt-link>
+              </li>
+              <div class="w-full bg-primary text-white p-4 dropdown-content">
+                <div class="flex container mx-auto">
+                  <form action="">
+                    <div class="w-11/12 my-4 pr-4">
+                      <!-- 彈跳視窗 -->
+                      <nuxt-link to="/">From</nuxt-link>
+                      <!-- 相反箭頭 -->
+                      <!-- 彈跳視窗 -->
+                      <nuxt-link to="/">To</nuxt-link>
+                      <!-- 下拉式 -->
+                      <select>
+                        <option value="Round Trip">Round Trip</option>
+                        <option value="OneWay">One Way</option>
+                        <option value="Multi-City">Multi-City</option>
+                      </select>
+                      <!-- 日期 -->
+                      <!-- 下拉式 -->
+                      <select>
+                        <option value="1Passenger">1 Passenger</option>
+                        <option value="2Passengers">2 Passengers</option>
+                        <option value="3Passengers">3 Passengers</option>
+                        <option value="4Passengers">4 Passengers</option>
+                        <option value="5Passengers">5 Passengers</option>
+                        <option value="6Passengers">6 Passengers</option>
+                        <option value="7Passengers">7 Passengers</option>
+                        <option value="8Passengers">8 Passengers</option>
+                        <option value="9Passengers">9 Passengers</option>
+                      </select>
+
+                      <!-- ---------------- -->
+
+                      <!-- 勾選欄位+彈跳視窗 -->
+                      <!-- 勾選欄位+彈跳視窗 -->
+                      <!-- 勾選欄位 -->
+                    </div>
+                    <div class="text-white w-1/12 my-4 px-4 font-semibold">
+                      <!-- 按鈕 -->
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <li>
+              <nuxt-link to="/">CHECK-IN</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/">MY TRIPS</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/">FLIGHT</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/">STATUS</nuxt-link>
             </li>
           </div>
 
@@ -33,18 +88,12 @@
                     <div
                       class="flex w-6/12 text-text space-x-6 border-l-2 border-text px-2 my-4 font-medium"
                     >
-                      <ul class="space-y-2">
-                        <li v-for="(item, index3) in menu3" :key="index3">
-                          <nuxt-link to="/">{{ item }}</nuxt-link>
-                        </li>
-                      </ul>
-                      <ul class="space-y-2">
-                        <li v-for="(item, index4) in menu4" :key="index4">
-                          <nuxt-link to="/">{{ item }}</nuxt-link>
-                        </li>
-                      </ul>
-                      <ul class="space-y-2">
-                        <li v-for="(item, index5) in menu5" :key="index5">
+                      <ul class="space-y-2 column">
+                        <li
+                          v-for="(item, index3) in menu3"
+                          :key="index3"
+                          class="secondary-link"
+                        >
                           <nuxt-link to="/">{{ item }}</nuxt-link>
                         </li>
                       </ul>
@@ -77,20 +126,8 @@
                     <div
                       class="flex w-6/12 text-text space-x-6 border-l-2 border-text px-2 my-4 font-medium"
                     >
-                      <ul class="space-y-2">
+                      <ul class="space-y-2 column">
                         <li v-for="(item, index7) in menu7" :key="index7">
-                          <nuxt-link to="/">{{ item }}</nuxt-link>
-                        </li>
-                      </ul>
-
-                      <ul class="space-y-2">
-                        <li v-for="(item, index8) in menu8" :key="index8">
-                          <nuxt-link to="/">{{ item }}</nuxt-link>
-                        </li>
-                      </ul>
-
-                      <ul class="space-y-2">
-                        <li v-for="(item, index9) in menu9" :key="index9">
                           <nuxt-link to="/">{{ item }}</nuxt-link>
                         </li>
                       </ul>
@@ -100,7 +137,44 @@
                 <!-- Dropdown -->
               </div>
             </li>
-            <li><nuxt-link to="/Help" class="pb-6">Need Help?</nuxt-link></li>
+            <li>
+              <div class="dropbtn">
+                <nuxt-link to="/Help" class="pb-6">Need Help?</nuxt-link>
+                <!-- Dropdown -->
+                <div class="w-full bg-primary text-white p-4 dropdown-content">
+                  <div class="flex container mx-auto">
+                    <div class="w-3/12 my-4 pr-4">
+                      <img
+                        class="img-fluid secondNavImage"
+                        src="//content.delta.com/content/www/us/en.damAssetRender.20200619T1448117200400.html/content/dam/delta_homepage_redesign/travelinfo/delta-one-head-rest.png"
+                        alt="Delta One®"
+                      />
+                    </div>
+                    <div class="text-white w-3/12 my-4 px-4 font-semibold">
+                      <ul class="space-y-2">
+                        <li v-for="(item, index4) in menu4" :key="index4">
+                          <nuxt-link to="/">{{ item }}</nuxt-link>
+                        </li>
+                      </ul>
+                    </div>
+                    <div
+                      class="flex w-6/12 text-text space-x-6 border-l-2 border-text px-2 my-4 font-medium"
+                    >
+                      <ul class="space-y-2 column">
+                        <li
+                          v-for="(item, index5) in menu5"
+                          :key="index5"
+                          class="secondary-link"
+                        >
+                          <nuxt-link to="/">{{ item }}</nuxt-link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <!-- Dropdown -->
+              </div>
+            </li>
           </div>
 
           <div class="flex font-bold space-x-4">
@@ -124,108 +198,112 @@
     </div>
 
     <Nuxt />
-    <div class="bg-primary text-white font-semibold p-8">
-      <div class="space-y-10">
-        <div class="text-center border-b-2 border-text pb-16 space-y-4 mx-24">
+    <div class="bg-primary text-white font-semibold">
+      <div class="container mx-auto">
+        <div class="text-center border-b-2 border-text pb-10 space-y-4">
           <div class="text-5xl"><button>Search for a topic...</button></div>
           <div class="space-x-6">
             <span class="text-text">Popular Topics:</span>
-            <span><nuxt-link to="/search">Need Help?</nuxt-link></span>
-            <span><nuxt-link to="/search">Coronavirus</nuxt-link></span>
-            <span><nuxt-link to="/search">eCredits</nuxt-link></span>
+            <span v-for="(item, index8) in menu8" :key="index8">
+              <nuxt-link to="/">{{ item }}</nuxt-link>
+            </span>
           </div>
         </div>
 
-        <div class="flex justify-center space-x-32">
+        <div class="flex justify-around pt-12" id="menu1">
           <div>
-            <div class="text-text font-bold mb-6">
+            <div class="text-text font-bold mb-6 text-lg">
               ABOUT DELTA
             </div>
-            <div class="space-y-2 text-sm">
-              <div><nuxt-link to="/search">About Us</nuxt-link></div>
-              <div><nuxt-link to="/search">Careers</nuxt-link></div>
-              <div><nuxt-link to="/search">News Hub</nuxt-link></div>
-              <div>
-                <nuxt-link to="/search">Investor Relations</nuxt-link>
-              </div>
-              <div><nuxt-link to="/search">Business Travel</nuxt-link></div>
-              <div><nuxt-link to="/search">Travel Agents</nuxt-link></div>
-              <div><nuxt-link to="/search">Mobile App</nuxt-link></div>
+            <div class="">
+              <ul class="space-y-2">
+                <li v-for="(item, index9) in menu9" :key="index9">
+                  <nuxt-link to="/search">{{ item }}</nuxt-link>
+                </li>
+              </ul>
             </div>
           </div>
 
           <div>
-            <div class="text-text font-bold mb-6">
+            <div class="text-text font-bold mb-6 text-lg">
               CUSTOMER SERVICE
             </div>
-            <div class="space-y-2 text-sm">
-              <div><nuxt-link to="/search">Need Help?</nuxt-link></div>
-              <div><nuxt-link to="/search">Tweet Us</nuxt-link></div>
-              <div><nuxt-link to="/search">Comment/Complaint</nuxt-link></div>
+            <div class="">
+              <ul class="space-y-2">
+                <li v-for="(item, index10) in menu10" :key="index10">
+                  <nuxt-link to="/">{{ item }}</nuxt-link>
+                </li>
+              </ul>
             </div>
           </div>
 
           <div>
-            <div class="text-text font-bold mb-6">
+            <div class="text-text font-bold mb-6 text-lg">
               SITE SUPPORT
             </div>
-            <div class="space-y-2 text-sm">
-              <div><nuxt-link to="/search">Login Help</nuxt-link></div>
-              <div><nuxt-link to="/search">Site Map</nuxt-link></div>
-              <div>
-                <nuxt-link to="/search">Browser Compatibility</nuxt-link>
-              </div>
-              <div><nuxt-link to="/search">Accessibility</nuxt-link></div>
-              <div>
-                <nuxt-link to="/search">Booking Information</nuxt-link>
-              </div>
+            <div class="">
+              <ul class="space-y-2">
+                <li v-for="(item, index11) in menu11" :key="index11">
+                  <nuxt-link to="/">{{ item }}</nuxt-link>
+                </li>
+              </ul>
             </div>
           </div>
 
           <div>
-            <div class="text-text font-bold mb-6">
+            <div class="text-text font-bold mb-6 text-lg">
               POLICIES
             </div>
-            <div class="space-y-2 text-sm">
-              <div>
-                <nuxt-link to="/search">Customer Commitment</nuxt-link>
-              </div>
-              <div><nuxt-link to="/search">Tarmac Delay Plan</nuxt-link></div>
-              <div><nuxt-link to="/search">Legal</nuxt-link></div>
-              <div><nuxt-link to="/search">Sustainability</nuxt-link></div>
-              <div>
-                <nuxt-link to="/search">Contract of Carriage</nuxt-link>
-              </div>
-              <div>
-                <nuxt-link to="/search">Cookies, Privacy & Security</nuxt-link>
-              </div>
-              <div>
-                <nuxt-link to="/search">Human Trafficking Statement</nuxt-link>
-              </div>
+            <div class="">
+              <ul class="space-y-2">
+                <li v-for="(item, index12) in menu12" :key="index12">
+                  <nuxt-link to="/">{{ item }} </nuxt-link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
-        <div class="">
-          <div class="size text-text text-center mb-4">
+        <!--© 2020 Delta Air Lines-->
+        <div class="mt-8">
+          <div class="text-sm text-text text-center mb-4">
             © 2020 Delta Air Lines, Inc. | Travel may be on other airlines.
           </div>
-          <div class="size text-text text-center">
+          <div class="text-sm text-text text-center">
             Terms and conditions apply to all offers and SkyMiles benefits. See
             specific offer for details, and visit
-            <span class="size text-white"
-              >SkyMiles Membership Guide & Program Rules Website Feedback</span
-            >
+            <span class="text-sm text-white"
+              ><nuxt-link to="/">
+                SkyMiles Membership Guide & Program Rules</nuxt-link
+              >
+            </span>
+          </div>
+        </div>
+        <!--© 2020 Delta Air Lines-->
+
+        <!--Website Feedback-->
+        <div class="mt-8">
+          <div class="text-center"><a href="">Website Feedback</a></div>
+
+          <div class="flex text-2xl space-x-4 p-4">
+            <div class="w-1/2 flex justify-end space-x-4">
+              <nuxt-link to="/" class="rounded-full border-1 px-4 pt-2"
+                ><fa :icon="['fab', 'facebook-f']"
+              /></nuxt-link>
+              <nuxt-link to="/" class="rounded-full border-1 px-3 py-2"
+                ><fa :icon="['fab', 'twitter']"
+              /></nuxt-link>
+            </div>
+
+            <div class="w-1/2 border-l-2 pl-4 text-4xl border-gray-300">
+              <nuxt-link to="/">
+                <fa :icon="['fas', 'globe']" class="rounded-full border-2" /> -
+              </nuxt-link>
+            </div>
           </div>
         </div>
 
-        <div>
-          <div class="text-center"><a href="">Website Feedback</a></div>
-        </div>
-        <div class="flex">
-          <div></div>
-          <div></div>
-        </div>
+        <!--Website Feedback-->
       </div>
     </div>
   </div>
@@ -251,21 +329,37 @@ export default {
         "Check-in & Security",
         "Delta Sky Club®",
         "Airport Maps & Locatios",
-      ],
-      menu4: [
         "Flight Deals",
         "Flight Schedules",
         "Destinations",
         "Onboard Experience",
         "Gift Cards",
         "Aircraft",
-      ],
-      menu5: [
         "Delta Vacations",
         "Rent A Car",
         "Shop Hotels",
         "In-Flight Wi-Fi",
         "Trip Protection",
+      ],
+      menu4: [
+        "Change/Cancel Help",
+        "Refund Help",
+        "Seats Help",
+        "Baggage Help",
+        "Comment/Complaint",
+      ],
+      menu5: [
+        "Coronavirus Updates ",
+        "Coronavirus Travel FAQs",
+        "Need Help?",
+        "Receipts",
+        "Certificates & eCredits",
+        "Accessible Travel Services",
+        "Child&Infant Travel",
+        "Pet Travel",
+        "Special Circumstances",
+        "SkyMiles Help",
+        "Mobile",
       ],
       menu6: [
         "My SkyMiles",
@@ -278,36 +372,55 @@ export default {
         "Program Resources",
         "Buy or Transfer Miles",
         "SkyMiles Credit Cards",
-      ],
-      menu8: [
         "SkyMiles Partners",
         "News & Special Offers",
         "SkyMiles Cruises",
         "Book Award Travel",
-      ],
-      menu9: [
         "Upgrade With Miles",
         "Manage your Account",
         "Certificates & eCredits",
       ],
-      menu10: [],
+      menu8: ["Need Help?", "Coronavirus", "eCredits"],
+      menu9: [
+        "About Us",
+        "Careers",
+        "News Hub",
+        "Investor",
+        "Relations",
+        "Business Travel",
+        "Travel Agents",
+        "Mobile App",
+      ],
+      menu10: ["Need Help?", "Tweet Us", "Comment/Complaint"],
+      menu11: [
+        "Login Help",
+        "Site Map",
+        "Browser Compatibility",
+        "Accessibility",
+        "Booking Information",
+      ],
+      menu12: [
+        "Customer Commitment",
+        "Tarmac Delay Plan",
+        "Legal",
+        "Sustainability",
+        "Contract of Carriage",
+        "Cookies,Privacy & Security",
+        "Human Trafficking Statement",
+      ],
+      menu13: [],
+      menu14: [],
     };
   },
-
-  // name: "menu2",
-  // data() {
-  //   return {
-
-  //   };
-  // },
+  methods: {
+    clearViewModel() {
+      console.log("this");
+    },
+  },
 };
 </script>
 
 <style>
-.size {
-  font-size: 5px;
-}
-
 html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -325,34 +438,5 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
