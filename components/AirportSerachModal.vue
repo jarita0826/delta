77f4e14cde6631632b8f800280c9a6077cf6991f
jarita <jarita0826@gmail.com>
2021-01-11 18:17:57 +0800

    add components

---
+++ components/AirportSerachModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-white" v-show="value">
+    <div class="modal px-8 py-6">
+      <div class="flex item-center justify-end">
+        <button @click.prevent="close">
+          <fa :icon="['fas', 'times']" class="text-text" />
+        </button>
+      </div>
+
+      <div class="p-3">
+        <ul class="border-b-2 border-text">
+          <li>
+            <button
+              class="w-1/3 text-primary text-center font-bold border-b-2 border-red pb-2"
+            >
+              SEARCH
+            </button>
+          </li>
+        </ul>
+
+        <div class="mt-6 text-primary">
+          {{ title }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+    value: {
+      required: true,
+    },
+  },
+  data() {},
+  methods: {
+    close() {
+      this.$emit("input", !this.value);
+    },
+  },
+
+  computed: {},
+};
+</script>
---
+++ components/Passenger.vue
@@ -0,0 +1,22 @@
+<template>
+  <select class="bg-primary border-b-1 text-2xl border-white p-2 text-red h-20">
+    <option value="1Passenger">1 Passenger</option>
+    <option value="2Passengers">2 Passengers</option>
+    <option value="3Passengers">3 Passengers</option>
+    <option value="4Passengers">4 Passengers</option>
+    <option value="5Passengers">5 Passengers</option>
+    <option value="6Passengers">6 Passengers</option>
+    <option value="7Passengers">7 Passengers</option>
+    <option value="8Passengers">8 Passengers</option>
+    <option value="9Passengers">9 Passengers</option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
